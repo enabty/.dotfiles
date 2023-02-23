c5cb27b31b7fe1239c0d855256b5d9b4078a0e03
enabty <ybring@student.chalmers.se>
2023-02-23 19:39:00 +0100

    Added current clipboard (wl-paste) to fish_greeting

--- dot_config/private_fish/functions/private_fish_greeting.fish
+++ dot_config/private_fish/functions/private_fish_greeting.fish
@@ -1,8 +1,11 @@
 function fish_greeting
   set -f row1 '🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟'
-  # set -f row2 '🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟'
+  set -f row2 '🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟🐟'
+  set -f gstr 'wl-paste: '
+
   echo $row1
   # tmux list-sessions
+  echo $gstr $(wl-paste)
   # echo \n
   echo $row2
 end
