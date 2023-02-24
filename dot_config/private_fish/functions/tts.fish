c74aa6378c4a90e9dc8fb2e344d7dc0bb3973ab8
enabty <ybring@student.chalmers.se>
2023-02-24 11:17:57 +0100

    added tts function wrapper for timetracer -status ..

---
+++ dot_config/private_fish/functions/tts.fish
@@ -0,0 +1,10 @@
+function tts
+  set -f status_output (timetrace status -o json)
+  set -f project (echo $status_output | jq -r '.project')
+  set -f tracked_time_today (echo $status_output | jq -r '.trackedTimeToday')
+  set -f tracked_time_current (echo $status_output | jq -r '.trackedTimeCurrent')
+
+  echo "Current project: $project"
+  echo "Worked today: $tracked_time_today"
+  echo "Worked on current task: $tracked_time_current"
+end
