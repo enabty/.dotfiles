e027d41e47ea3107cc014ad47c9d4602344a206b
enabty <ybring@student.chalmers.se>
2023-02-24 20:07:16 +0100

    added psm and psc functions

--- dot_config/private_fish/conf.d/abbrs.fish
+++ dot_config/private_fish/conf.d/abbrs.fish
@@ -3,6 +3,12 @@ abbr -a ls 'exa'
 abbr -a rmrf 'rm -rf'
 abbr -a exg 'exa -al | grep'
 
+# PS
+abbr -a psm10 'ps -eo pid,ppid,cmd,comm,%mem,%cpu --sort=-%mem | head -10'
+abbr -a psm50 'ps -eo pid,ppid,cmd,comm,%mem,%cpu --sort=-%mem | head -50'
+abbr -a psc10 'ps -eo pid,ppid,cmd,comm,%mem,%cpu --sort=-%cpu | head -10'
+abbr -a psc50 'ps -eo pid,ppid,cmd,comm,%mem,%cpu --sort=-%cpu | head -50'
+
 # Navigation
 abbr -a dlds 'cd ~/Downloads/'
 abbr -a docs 'cd ~/Documents/'
