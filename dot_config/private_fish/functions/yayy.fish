e8cafc3172e06be4858a222f96dbdfa3085504df
enabty <ybring@student.chalmers.se>
2023-02-27 10:49:33 +0100

    added bool to check if it should print or not

--- dot_config/private_fish/functions/yayy.fish
+++ dot_config/private_fish/functions/yayy.fish
@@ -2,27 +2,31 @@ function yayy
   set -f package (echo "$argv[2]" | cut -d' ' -f1)
   set -f action "$argv[1]"
   set -f yayy_file ~/.config/yayy.txt
+  set -f install_bool false
 
   if test "$action" = "-S"
-      yay -S $package
-      if yay -Qi $package >/dev/null 2>&1
-          echo "$package $(date '+%d-%m-%Y %H:%M:%S')" >> $yayy_file
-          set -f output_str "Successfully installed package $package"
-      end
+    yay -S $package
+    if yay -Qi $package >/dev/null 2>&1
+        echo "$package $(date '+%d-%m-%Y %H:%M:%S')" >> $yayy_file
+        set -f output_str "Successfully installed package $package"
+        set -f install_bool true
+    end
   else if test "$action" = "-Rs"
-      if yay -Rp $package >/dev/null 2>&1
-          if yay -Rs $package | tee /dev/tty; and sed -i "/^$package/d" $yayy_file
-              set -f output_str "Successfully removed package $package"
-          else
-              set -f output_str "Failed to remove package $package"
-          end
+    if yay -Rp $package >/dev/null 2>&1
+      if yay -Rs $package | tee /dev/tty; and sed -i "/^$package/d" $yayy_file
+          set -f output_str "Successfully removed package $package"
       else
-          set -f output_str "Package $package not found."
+          set -f output_str "Failed to remove package $package"
       end
+    else
+        set -f output_str "Package $package not found."
+    end
   else
       yay $argv
   end
-  echo "Done.."
-  echo $output_str
-  echo (tail -n 1 $yayy_file)
+  if test $install_bool = true
+    echo "Done.."
+    echo $output_str
+    echo (tail -n 1 $yayy_file)
+  end
 end
